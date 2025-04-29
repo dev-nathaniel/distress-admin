3870d4a7bd1e1aa5bb3c8393eb628acc4ef5e3cc
Olowodev <nathanielolowofoyeku@gmail.com>
2025-04-29 14:22:33 +0100

    added map screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,126 @@
+"use client"
 import Image from "next/image";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import axios from "axios";
+
+
+const alerts = [1, 2, 3, 4, 5, 6, 7, 8]
+
 
 export default function Home() {
+  const mapContainer = useRef<any>(null)
+  const map = useRef<mapboxgl.Map | any>(null);
+  const [selectedDistress, setSelectedDistress] = useState()
+  mapboxgl.accessToken = 'pk.eyJ1Ijoib2xvd29hIiwiYSI6ImNsZjNyMndhcTBnNm8zcm50cmFkZzI1NXAifQ.sUHuNAw9DIe1ATZcaV_ETg';
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current, // container ID
+      style: 'mapbox://styles/olowoa/cm9igyzv100rr01s84bmmhc6f', // style URL
+      center: [-1.1398, 52.6369], // starting position [lng, lat]
+      zoom: 15, // starting zoom
+    });
+  }, [])
+
+  const deploy = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/deploy')
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.data)
+    }
+  }
+
+  const selectDistress = async (data) => {
+    setSelectedDistress(data)
+    // requestDirection()
+  }
+
+  const requestDirection = async () => {
+    try {
+      const response = await axios.get(`https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=pk.eyJ1Ijoib2xvd29hIiwiYSI6ImNsZjNyMndhcTBnNm8zcm50cmFkZzI1NXAifQ.sUHuNAw9DIe1ATZcaV_ETg`)
+      console.log(response.data)
+      const data = response.data
+      const route = data.routes[0]
+      const geometry = route.geometry
+      const geojson = {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': geometry
+      };
+
+      if (map.current.getSource('route')) {
+        // if the route already exists on the map, reset it using setData
+        map.current.getSource('route').setData(geojson);
+      }
+
+      // otherwise, add a new layer using this data
+      else {
+        map.current.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: geojson
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          paint: {
+            'line-color': '#C60000',
+            'line-width': 5,
+            'line-opacity': 1
+          }
+        });
+      }
+      // new mapboxgl.Map({}).flyTo()
+      map.current.flyTo({
+        center: [-84.518641,39.134270],
+        zoom: 15,
+        speed: 1,
+        curve: 1,
+        easing(t) {
+            return t;
+        }
+    })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative" id="map">
+      <div className="w-dvw h-dvh relative" ref={mapContainer}>
+        {/* <div className="absolute w-full h-full items-center justify-end flex px-8"> */}
+
+        <div className="absolute right-8 top-1/2 -translate-y-1/2 h-11/12 w-1/4 z-50 rounded-3xl bg-[#000000BF] py-6 overflow-y-scroll [&::-webkit-scrollbar]:w-0 gap-6 flex flex-col">
+          <p className="font-bold text-[24px] px-6">Alerts</p>
+          {alerts.map((data, index) => (
+            <div key={index} onClick={() => selectDistress(data)} className={selectedDistress == data ? "flex-col flex gap-2 bg-[#7a7a7a50] px-6" : "flex-col flex gap-2 px-6"}>
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-[16px] text-[#C60000]">TYRONE IN DISTRESS</p>
+                <div className="w-4 h-4 rounded-4xl bg-[#C60000]"></div>
+              </div>
+              <div className="flex gap-4">
+                <p className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap font-medium text-[#E9E9E9]">From “Stark Tower, Sentinel street, 3rd district” </p>
+                <p className="text-[#E9E9E9] font-medium">2 mins ago</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <div onClick={deploy} className="bg-[#C60000] px-4 py-1 rounded-2xl cursor-pointer">
+                  <p>Deploy</p>
+                </div>
+                <p className="text-[#7A7A7A] underline">View Feedback</p>
+              </div>
+            </div>
+          ))}
+
+        </div>
+        {/* </div> */}
+      </div>
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -98,6 +216,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+    </div> */}
     </div>
   );
 }
